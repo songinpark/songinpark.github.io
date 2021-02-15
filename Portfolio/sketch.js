7b9e791fb37a0b1cee349e9110b2470f669c4c45
songinpark <68854347+songinpark@users.noreply.github.com>
2021-02-15 15:15:31 +0100

    Add files via upload

---
+++ Portfolio/sketch.js
@@ -0,0 +1,64 @@
+
+var Engine = Matter.Engine,
+//Render = Matter.Render,
+World = Matter.World,
+Bodies = Matter.Bodies;
+
+var engine;
+var world;
+var boxy;
+var Render = Matter.Render,
+Runner = Matter.Runner,
+Common = Matter.Common,
+MouseConstraint = Matter.MouseConstraint,
+Mouse = Matter.Mouse,
+Vertices = Matter.Vertices,
+Svg = Matter.Svg;
+var ground;
+var options = {
+    isStatic: true
+}
+var loadSvg;
+
+function setup(){
+    createCanvas (400, 400);
+    engine = Engine.create();
+    world = engine.world;
+    boxy = Bodies.rectangle(200, 100, 80, 80);
+    Engine.run(engine);
+    World.add(world,boxy);
+    console.log(boxy);
+    ground = Bodies.rectangle(200, height, width, 78, options);
+    World.add(world, ground);
+    
+}
+
+function draw() {
+    background(51);
+    console.log(boxy.position.x);
+    rect (boxy.position.x, boxy.position.y, 80, 80);
+
+}
+
+
+loadSvg = function(url) {
+    return fetch(url)
+        .then(function(response) { return response.text(); })
+        .then(function(raw) { return (new window.DOMParser()).parseFromString(raw, 'image/svg+xml'); });
+};
+
+
+loadSvg('./svg/e_spree.svg').then(function(root) {
+    var color = Common.choose(['#f19648', '#f5d259', '#f55a3c', '#063e7b', '#ececd1']);
+    
+    var vertexSets = select(root, 'path')
+        .map(function(path) { return Svg.pathToVertices(path, 30); });
+
+    World.add(world, Bodies.fromVertices(400, 80, vertexSets, {
+        render: {
+            fillStyle: color,
+            strokeStyle: color,
+            lineWidth: 1
+        }
+    }, true));
+});
